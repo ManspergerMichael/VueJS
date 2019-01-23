7ae10f3f57b7112b5017d828ab8c29e6ed6bf80d
Michael Mansperger <ManspergerMichael@gmail.com>
2019-01-23 12:08:14 -0800

    finished for the day

---
+++ Vue Class 2/scripts.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: '#app',
+    data: {
+        counter: 0,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        incement: function(step, event)
+        {
+            this.counter += step;
+        },
+        updateCoordinates: function(event)
+        {
+            //clientX and clientY are default proerties of event
+            this.x = event.clientX;
+            this.y = event.clientY;
+        },
+        dummy: function(event)
+        {
+            event.stopPropagation();
+        },
+        alertMe: function()
+        {
+            alert('Alert!');
+        }
+    }
+});
