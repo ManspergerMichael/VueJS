93f5836a68564fd9454a8a053dc2c3a23f712564
Michael Mansperger <ManspergerMichael@gmail.com>
2019-01-22 16:01:50 -0800

    assignment 2 finished

--- Assignment2/Exercise 2/app.js
+++ Assignment2/Exercise 2/app.js
@@ -2,5 +2,18 @@ new Vue({
         el: '#exercise',
         data: {
             value: ''
+        },
+        methods: {
+            alert: function(){
+                alert('ALARM!');
+            },
+            store: function(event)
+            {
+                this.value = event.target.value;
+            },
+            complete: function(event)
+            {
+                this.value = event.target.value;
+            }
         }
     });
