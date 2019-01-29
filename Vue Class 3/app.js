08930bda9ee4d2bf6d4cc18ea7942b7f4e79d1a9
Michael Mansperger <ManspergerMichael@gmail.com>
2019-01-29 15:18:43 -0800

    tutorial 3

--- Vue Class 3/app.js
+++ Vue Class 3/app.js
@@ -4,3 +4,17 @@ new Vue({
         name: 'Max'
     }
 })
+
+new Vue({
+    el: '#app2',
+    data: {
+        counter: 0,
+        result: ''
+    },
+    methods: {
+        increase: function(){
+            this.counter ++;
+            this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+        }
+    }
+})
