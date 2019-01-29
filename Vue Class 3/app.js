a9238199592adbc7dfbec978167ee6935017611e
Michael Mansperger <ManspergerMichael@gmail.com>
2019-01-29 15:41:00 -0800

    starting computed properties

--- Vue Class 3/app.js
+++ Vue Class 3/app.js
@@ -9,17 +9,24 @@ new Vue({
     el: '#app2',
     data: {
         counter: 0,
-        result: ''
+        secondCounter: 0
+        //result: ''
+    },
+    computer: {
+
     },
     methods: {
         //cross propertiy dependanc
-        increase: function(){
+        /* increase: function(){
             this.counter ++;
             this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
         },
         decrease: function(){
             this.counter --;
             this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+        } */
+        result() {
+            return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5'
         }
     }
 })
