2ff0253af2528277228f79d2abf69b8848805113
Michael Mansperger <ManspergerMichael@gmail.com>
2019-02-06 14:45:20 -0800

    computed property
    Using a computed property to pass a javaScript object to change the class on click.

--- Vue Class 4/app.js
+++ Vue Class 4/app.js
@@ -2,5 +2,14 @@ new Vue({
     el:'#app',
     data: {
         attachRed: false
+    },
+    computed:{
+        divClasses: function(){
+            //returns a javaScript object
+            return {
+                red: this.attachRed,
+                blue: !this.attachRed
+            };
+        }
     }
 });
